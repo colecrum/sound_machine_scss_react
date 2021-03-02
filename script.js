b4eaf41569763d355b66f91c5c9b5b837fb28239
Cole Crum <68294806+colecrum@users.noreply.github.com>
2021-03-02 07:15:36 -0600

    commit3
    responsive mobile and scroll repair

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Variables------------------------------------
+
 //sound bank 1
 const bank1 = [
 {
@@ -124,6 +126,7 @@ const inactivePad = {
   color: "magenta" };
 
 
+//Components ----------------------------------------
 
 //DrumPad component
 class DrumPad extends React.Component {
@@ -281,11 +284,14 @@ class App extends React.Component {
       React.createElement("section", { id: "contact", class: "container" }, /*#__PURE__*/
       React.createElement("h2", { id: "contact-title" }, "Designed & Coded by Cole Crum"), /*#__PURE__*/
       React.createElement("div", { class: "contact-links" }, /*#__PURE__*/
-
-      React.createElement("a", { id: "profile-link", href: "https://github.com/colecrum?tab=repositories", target: "_blank", class: "btn contact-details" }, /*#__PURE__*/React.createElement("img", { id: "git-logo", class: "contact-img", src: "https://www.sferalabs.cc/wp-content/uploads/github-logo-white.png", title: "GitHub" })), /*#__PURE__*/
-      React.createElement("a", { href: "https://codepen.io/colecrum", target: "_blank", class: "btn contact-details" }, /*#__PURE__*/React.createElement("img", { class: "contact-img", src: "https://blog.codepen.io/wp-content/uploads/2012/06/Button-White-Large.png", title: "CodePen" })), /*#__PURE__*/
-      React.createElement("a", { href: "mailto:colemcrum@gmail.com", target: "_blank", class: "btn contact-details" }, /*#__PURE__*/React.createElement("img", { class: "contact-img", src: "https://lh3.googleusercontent.com/VS3B_qhOFTYsdyNfnlr98zg3HNjB_Gcs9bxVnaQO9MysAoBOXMHATClhRviImKKJV8RV-0s7hl8KeVQcij5Iagb1exHzt40x679l8Q=w0", title: "Email" })), /*#__PURE__*/
-      React.createElement("a", { href: "tel:512-517-8503", target: "_blank", class: "btn contact-details" }, /*#__PURE__*/React.createElement("img", { class: "contact-img", src: "https://cdn4.iconfinder.com/data/icons/phones-colored/48/JD-32-512.png", title: "Phone" }))), "\xA0 \xA0 \xA0 "), "\xA0 \xA0 ")));
+      React.createElement("div", { class: "link-cont", id: "git-cont" }, /*#__PURE__*/
+      React.createElement("a", { id: "profile-link", href: "https://github.com/colecrum?tab=repositories", target: "_blank", class: "btn contact-details" }, /*#__PURE__*/React.createElement("img", { id: "git-logo", class: "contact-img", src: "https://www.sferalabs.cc/wp-content/uploads/github-logo-white.png", title: "GitHub" }))), /*#__PURE__*/
+      React.createElement("div", { class: "link-cont", id: "pen-cont" }, /*#__PURE__*/
+      React.createElement("a", { href: "https://codepen.io/colecrum", target: "_blank", class: "btn contact-details" }, /*#__PURE__*/React.createElement("img", { class: "contact-img", src: "https://blog.codepen.io/wp-content/uploads/2012/06/Button-White-Large.png", title: "CodePen" }))), /*#__PURE__*/
+      React.createElement("div", { class: "link-cont", id: "mail-cont" }, /*#__PURE__*/
+      React.createElement("a", { href: "mailto:colemcrum@gmail.com", target: "_blank", class: "btn contact-details" }, /*#__PURE__*/React.createElement("img", { class: "contact-img", src: "https://lh3.googleusercontent.com/VS3B_qhOFTYsdyNfnlr98zg3HNjB_Gcs9bxVnaQO9MysAoBOXMHATClhRviImKKJV8RV-0s7hl8KeVQcij5Iagb1exHzt40x679l8Q=w0", title: "Email" }))), /*#__PURE__*/
+      React.createElement("div", { class: "link-cont", id: "phone-cont" }, /*#__PURE__*/
+      React.createElement("a", { href: "tel:512-517-8503", target: "_blank", class: "btn contact-details" }, /*#__PURE__*/React.createElement("img", { class: "contact-img", src: "https://cdn4.iconfinder.com/data/icons/phones-colored/48/JD-32-512.png", title: "Phone" })))), "\xA0 \xA0 \xA0 "), "\xA0 \xA0 ")));
 
 
 
